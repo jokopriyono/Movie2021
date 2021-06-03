2a899a86a301c50b2f025d3d2b5e41589cb99b86
jokopriyono <joe.mandrake007@gmail.com>
2021-06-03 11:37:05 +0700

    adding dependencies

---
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -0,0 +1,14 @@
+object BuildConfig {
+    const val kotlin= "1.3.31"
+    const val agp = "4.2.1"
+    const val googleServices = "4.2.0"
+    const val fabricTools = "1.29.0"
+
+    const val compileSdk = 28
+    const val minSdk = 22
+    const val targetSdk = 28
+
+    const val applicationId = "com.joko.movie2021"
+    const val versionCode = 1
+    const val versionName = "1.0.0"
+}
