cf3ec890f7af37cae175ad218982db6461b4135b
jokopriyono <joe.mandrake007@gmail.com>
2021-06-04 14:40:05 +0700

    add home screen fragment

---
+++ app/src/main/java/com/joko/movie2021/ui/main/HomeFragment.kt
@@ -0,0 +1,18 @@
+package com.joko.movie2021.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.joko.movie2021.R
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
--- app/src/main/java/com/joko/movie2021/ui/main/MainActivity.kt
+++ app/src/main/java/com/joko/movie2021/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.joko.movie2021
+package com.joko.movie2021.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.joko.movie2021.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
