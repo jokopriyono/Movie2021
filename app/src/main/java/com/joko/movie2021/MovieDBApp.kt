2822a26c5915f215d1b807b6e70260aad96130cb
jokopriyono <joe.mandrake007@gmail.com>
2021-06-06 17:08:27 +0700

    add left rotation algorithm

--- app/src/main/java/com/joko/movie2021/MovieDBApp.kt
+++ app/src/main/java/com/joko/movie2021/MovieDBApp.kt
@@ -18,5 +18,19 @@ class MovieDBApp : Application() {
 //                )
 //            )
 //        }
+
+        // ====Run left rotation=======================================
+        val arr = listOf(2, 3, 5, 1, 2, 3, 9, 8)
+        val numOfRotation = 5
+        val resultLeftRotation = leftRotation(arr, numOfRotation)
+        println(resultLeftRotation)
+    }
+
+    private fun leftRotation(arr: List<Int>, numOfRotation: Int): List<Int> {
+        val temp = arr.toMutableList()
+        val cut = mutableListOf<Int>()
+        for (i in 0 until numOfRotation) cut.add(temp.removeAt(0))
+        temp.addAll(cut)
+        return temp
     }
 }
